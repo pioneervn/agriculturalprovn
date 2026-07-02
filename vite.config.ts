2edc0a186576479faff9e83d655d324385ac31ca
pioneervn <chuthanh035@gmail.com>
2026-07-02 10:18:49 +0700

    chore: remove base path from vite configuration
    Reset the base path to root to ensure correct asset resolution for the application deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/agriculturalprovn/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
