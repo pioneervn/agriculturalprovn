16f262ab7ae3adbd2590f15492fe85fc2fe2593d
pioneervn <chuthanh035@gmail.com>
2026-07-02 10:05:10 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/agriculturalprovn/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
